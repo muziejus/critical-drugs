6972cdd261e4fcc0d78b2e2b9fdb5a6762e0cbe1
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-10-12 17:23:36 -0400

    feat: flesh out /site endpoint on Mirage.

--- app/routes/site.ts
+++ app/routes/site.ts
@@ -2,13 +2,8 @@ import Route from "@ember/routing/route";
 
 export default class SiteRoute extends Route {
   async model() {
-    return {
-      omeka_url: "this.urlPrefix",
-      omeka_version: "2.7",
-      title: "Emb-Line Omeka Backend",
-      description: "Lorem ipsum dolo sit amet",
-      author: "The Emb-Line Team",
-      copyright: "Creative Commons Attribution-ShareAlike 3.0 License",
-    };
+    const response = await fetch("https://emb-line-omeka-site.com/api/site");
+    let data = await response.json();
+    return data;
   }
 }
