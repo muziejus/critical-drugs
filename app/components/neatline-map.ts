68f8ed0538a41181a2b242cb1d06209bc0d565c6
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2023-02-20 23:25:06 -0500

    feat: Hacky fix for leaflet tiles.

--- app/components/neatline-map.ts
+++ app/components/neatline-map.ts
@@ -1,5 +1,6 @@
 import Component from "@glimmer/component";
 import { service } from "@ember/service";
+import { action } from "@ember/object";
 import Store from "@ember-data/store";
 import NeatlineRecord from "emb-line/models/neatline-record";
 import NeatlineFilter from "emb-line/services/neatline-filter";
@@ -17,6 +18,13 @@ export default class NeatlineMapComponent extends Component<NeatlineMapComponent
   @service declare store: Store;
 
   @service declare neatlineFilter: NeatlineFilter;
+
+  @action onLoad(event) {
+    const map = event.target;
+    console.log("invalidating");
+    setTimeout(() => map.invalidateSize(), 0);
+    map.setView([39, -115], 3);
+  }
 
   get lat() {
     return this.args.lat ?? 39;
