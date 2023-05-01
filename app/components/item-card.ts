f8a706e22ee6bc01e1a1641182e0608340afcb30
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2023-05-01 10:38:20 -0400

    feat: add signature to ItemCard

--- app/components/item-card.ts
+++ app/components/item-card.ts
@@ -1,6 +1,12 @@
 import Component from "@glimmer/component";
+import ItemModel from "emb-line/models/item";
 
-interface ItemCardComponentSignature {}
+interface ItemCardComponentSignature {
+  Element: HTMLLIElement;
+  Args: {
+    item: ItemModel;
+  }
+}
 
 export default class ItemCardComponent extends Component<ItemCardComponentSignature> {
   get collection() {
