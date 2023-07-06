3fa610dcca416e20df3975f00f1f19d0dbf7092d
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2023-07-06 06:53:44 -0400

    feat: Implement query on the adapter so we can query items by item_type.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -45,6 +45,12 @@ export default class ApplicationAdapter extends RESTAdapter {
     return {};
   }
 
+  async query(store, type, query, recordArray, adapterOptions) {
+    const payload = await super.query(store, type, query, recordArray, adapterOptions);
+    return this.normalizePayload(payload, type)
+  }
+
+  // Let's not futz w/ this one for now.
   async findRecord(store, schema, queryId, snapshot) {
     // async findRecord(store: Store, schema: ModelSchema, queryId: string, snapshot: Snapshot) {
     const payload = await super.findRecord(store, schema, queryId, snapshot);
@@ -64,6 +70,36 @@ export default class ApplicationAdapter extends RESTAdapter {
     return out;
   }
 
+  normalizePayload(payload, schema) {
+    return {
+      data: payload.map(item => this.normalizeRecord(item, schema))
+    }
+  }
+
+  normalizeRecord(item, schema) {
+    const { id, ...attributes } = item;
+    if (attributes.element_texts) {
+      attributes.element_texts = this.formatElementTexts(
+        attributes.element_texts
+      );
+    }
+    const relationships = {};
+    for (const [relatedModel, i] of schema.relationships) {
+      relationships[relatedModel] = this.addRelatedModel(
+        relatedModel,
+        i,
+        item
+      );
+    }
+
+    return {
+      id,
+      type: schema.modelName,
+      attributes,
+      relationships,
+    };
+  }
+
   async findAll(
     store, //: Store,
     schema, //: ModelSchema,
@@ -77,31 +113,7 @@ export default class ApplicationAdapter extends RESTAdapter {
       sinceToken,
       snapshotRecordArray
     );
-    const out = {
-      data: payload.map(item => {
-        const { id, ...attributes } = item;
-        if (attributes.element_texts) {
-          attributes.element_texts = this.formatElementTexts(
-            attributes.element_texts
-          );
-        }
-        const relationships = {};
-        for (const [relatedModel, i] of schema.relationships) {
-          relationships[relatedModel] = this.addRelatedModel(
-            relatedModel,
-            i,
-            item
-          );
-        }
-        return {
-          id,
-          type: schema.modelName,
-          attributes,
-          relationships,
-        };
-      }),
-    };
-    return out;
+    return this.normalizePayload(payload, schema)
   }
 
   formatElementTexts(elementTexts) {
